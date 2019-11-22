afe4ec9bee661a3239e5179d1626da60677156bc
Leandro Reis <developer.reis@gmail.com>
2019-11-21 23:44:00 -0300

    ajuste docker aws

---
+++ ItauFileManager/ItauFileManager/src/main/java/br/com/itau/filemanager/entitys/FileStoredEntity.java
@@ -0,0 +1,37 @@
+package br.com.itau.filemanager.entitys;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_file_stored")
+public class FileStoredEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "file_name", nullable = false, length = 80)
+	private String fileName;
+	
+	@Column(name = "file_type", nullable = false, length = 80)
+	private String fileType;
+	
+	@Column(name = "file_uri", nullable = false, length = 80)
+	private String fileDownloadUri;
+	
+	@Column(name = "file_size", nullable = false)
+	private Long fileSize;
+}
